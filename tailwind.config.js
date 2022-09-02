e1aada1aa8735990cd8a47fe79accfc9968e4af7
Muhammad Alyan <alyan0332@gmail.com>
2022-09-02 20:52:51 +0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main: "url('/src/assets/images/bg-curvy-desktop.svg')",
+      },
+    },
   },
   plugins: [],
 };
